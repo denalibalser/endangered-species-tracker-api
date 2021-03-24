81cb0ba035c9d769f9f492c5e05fb651339b904c
denalibalser <denali.balser167@gmail.com>
2021-03-24 11:15:11 -0600

    added create and index actions to animal cards contoller

--- app/controllers/api/v1/animal_cards_controller.rb
+++ app/controllers/api/v1/animal_cards_controller.rb
@@ -1,2 +1,26 @@
-class AnimalCardsController < ApplicationController
+class Api::V1::AnimalCardsController < ApplicationController
+
+    def create
+        @animal_card = AnimalCard.new(animal_card_params)
+
+        if @animal_card.save
+            # @animal_card.user_id = current_user.id
+            render json: { status: 201, animal_card: @animal_card } 
+        else 
+            render json: { status: 401, animal_card: @animal_card, message: "Animal unable to be saved."}
+        end
+    end 
+
+    def index 
+        @animal_cards = AnimalCard.all
+
+        render json: { status: 201, animal_cards: @animal_cards}
+    end 
+
+    private 
+
+    def animal_card_params
+        params.require(:animal_card).permit(:common_name, :scientific_name, :endangered_level, :url, :user_id)
+
+    end
 end
