617fedf450e6cd9fb108cd716cd4007f53911fde
denalibalser <denali.balser167@gmail.com>
2021-06-03 13:03:48 -0500

    change cors configuration

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -1,12 +1,15 @@
 Rails.application.config.middleware.insert_before 0, Rack::Cors do
-    allow do
-      origins 'http://localhost:3002'
-      resource '*', headers: :any, methods: [:get, :post, :patch, :put, :delete], credentials: true
-    end
+    # allow do
+    #   origins 'http://localhost:3002'
+    #   resource '*', headers: :any, methods: [:get, :post, :patch, :put, :delete], credentials: true
+    # end
 
     # configuration for deployed version of app:
-    # allow do
-    #   origins 'heroku url' 
-    #   resource '*', headers: :any, methods: [:get, :post, :patch, :put, :delete] credentials: true
-    # end
+    allow do
+      origins '*'
+
+      resource '*', 
+      headers: :any, 
+      methods: [:get, :post, :patch, :put, :delete] credentials: true
+    end
 end
