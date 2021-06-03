74eb61e8f6ca7527a60a95f4a593b096428bc17f
denalibalser <denali.balser167@gmail.com>
2021-06-03 13:24:02 -0500

    fix syntax error in cors file

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -10,6 +10,6 @@ Rails.application.config.middleware.insert_before 0, Rack::Cors do
 
       resource '*', 
       headers: :any, 
-      methods: [:get, :post, :patch, :put, :delete] credentials: true
+      methods: [:get, :post, :patch, :put, :delete], credentials: true
     end
 end
