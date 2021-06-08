a9778a73c545cebd9f861dd01aec752776ef560a
denalibalser <denali.balser167@gmail.com>
2021-06-08 14:06:54 -0500

    change database config

--- db/schema.rb
+++ db/schema.rb
@@ -1,24 +1,51 @@
-ActiveRecord::Schema.define(version: 2021_03_22_162208) do
+# This file is auto-generated from the current state of the database. Instead
+# of editing this file, please use the migrations feature of Active Record to
+# incrementally modify your database, and then regenerate this schema definition.
+#
+# This file is the source Rails uses to define your schema when running `rails
+# db:schema:load`. When creating a new database, `rails db:schema:load` tends to
+# be faster and is potentially less error prone than running all of your
+# migrations from scratch. Old migrations may fail to apply correctly if those
+# migrations use external dependencies or application code.
+#
+# It's strongly recommended that you check this file into your version control system.
 
-  create_table "animal_cards", force: :cascade do |t|
-    t.string "common_name"
-    t.string "scientific_name"
-    t.string "endangered_level"
-    t.string "url"
-    t.integer "user_id"
+ActiveRecord::Schema.define(version: 0) do
+
+  # These are extensions that must be enabled in order to support this database
+  enable_extension "plpgsql"
+
+  create_table "restaurants", force: :cascade do |t|
+    t.string "name"
+    t.string "description"
+    t.string "address"
+    t.string "phone_number"
+    t.integer "vegan_friendliness_rating"
+    t.integer "price_rating"
+    t.bigint "user_id", null: false
+    t.bigint "state_id", null: false
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
-    t.index ["user_id"], name: "index_animal_cards_on_user_id"
+    t.index ["state_id"], name: "index_restaurants_on_state_id"
+    t.index ["user_id"], name: "index_restaurants_on_user_id"
   end
 
-  create_table "users", force: :cascade do |t|
-    t.string "username"
-    t.string "first_name"
-    t.string "last_name"
-    t.string "password_digest"
+  create_table "reviews", force: :cascade do |t|
+    t.text "body"
+    t.bigint "user_id", null: false
+    t.bigint "restaurant_id", null: false
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["restaurant_id"], name: "index_reviews_on_restaurant_id"
+    t.index ["user_id"], name: "index_reviews_on_user_id"
+  end
+
+  create_table "states", force: :cascade do |t|
+    t.string "name"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
   end
 
-  add_foreign_key "animal_cards", "users"
+  add_foreign_key "restaurants", "states"
+  add_foreign_key "reviews", "restaurants"
 end
